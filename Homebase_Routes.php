f55d02132cb007c9d6ddf5c5cce4e9daa3c7691c
Tyler Freeman <tyler.freeman@modea.com>
2015-02-26 13:50:15 -0500

    user schema end point response now displays field for foriegn key relationships

--- Homebase_Routes.php
+++ Homebase_Routes.php
@@ -99,8 +99,10 @@ class HomebaseApi extends RestfulAPI_Abs
        		$user_schema = array();
     		$tables = user_table_get();
     		foreach($tables as $table){
+          $table_name = $table['TABLE_NAME'];
     			$table['TABLE_COLS'] = user_table_column_get($table['TABLE_NAME']);
-          $table['TABLE_PRIMARY_KEY'] = user_schema_get_table_primary_key($table['TABLE_NAME']);
+          $table['TABLE_PRIMARY_KEY'] = user_schema_get_table_primary_key($table_name);
+          $table['TABLE_FOREIGN_KEY'] = user_schema_get_table_foreign_key($table_name);
     			$user_schema[] = $table;
     		}
     		$res['USER_SCHEMA'] = $user_schema;

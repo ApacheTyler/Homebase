0a9fbb437e541bbe0d9e77521ddb56627353d532
Tyler Freeman <tyler.freeman@modea.com>
2015-03-26 14:19:11 -0400

    fix: duplicate foreign keys

--- End_points/user_schema.php
+++ End_points/user_schema.php
@@ -31,10 +31,10 @@
 
 	function user_schema_get_table_foreign_key($table_name){
 		$results = executeQuery("SELECT c.constraint_name, a.table_name, a.column_name, a.table_name as reference_table, a.column_name as reference_column, c.deferrable, c.DEFERRED, c.DELETE_RULE
-                FROM all_cons_columns a
-                JOIN all_constraints c ON a.owner = c.owner
+                FROM user_cons_columns a
+                JOIN user_constraints c ON a.owner = c.owner
                     AND a.constraint_name = c.constraint_name
-								WHERE a.table_name = '$table_name'");
+								WHERE a.table_name = '$table_name' AND c.constraint_type='R'");
 		return $results;
 	}
 

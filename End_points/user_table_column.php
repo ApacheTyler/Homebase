ed491d5a88396d78ce0b59adeea670b3a0e0a134
Tyler Freeman <tyler.freeman@modea.com>
2015-03-29 14:41:55 -0400

    Added more detailed information to table columns

--- End_points/user_table_column.php
+++ End_points/user_table_column.php
@@ -1,7 +1,7 @@
 <?php
 	function user_table_column_get($req){
 		$table_name = (is_array($req)) ? $req['table-name'] : $req;
-		$results = executeQuery("SELECT column_name
+		$results = executeQuery("SELECT column_name, data_type, data_length, data_precision, data_scale
   						FROM user_tab_cols
   					  WHERE table_name = '$table_name'");
   	return $results;

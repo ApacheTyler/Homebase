5f2432c91d3cbce6badd0f2bd5a927721ed441b9
Tyler Freeman <tyler.freeman@modea.com>
2015-04-05 23:15:25 -0400

    Working on adding constraints

--- End_points/user_alter_table_constraint.php
+++ End_points/user_alter_table_constraint.php
@@ -1,15 +1,119 @@
 <?php
 
-  function user_alter_table_constraint_post($req){
+/** TURN OFF MAGIC QUOTES **/
+if (get_magic_quotes_gpc()) {
+	$process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+	while (list($key, $val) = each($process)) {
+			foreach ($val as $k => $v) {
+					unset($process[$key][$k]);
+					if (is_array($v)) {
+							$process[$key][stripslashes($k)] = $v;
+							$process[] = &$process[$key][stripslashes($k)];
+					} else {
+							$process[$key][stripslashes($k)] = stripslashes($v);
+					}
+			}
+	}
+	unset($process);
+}
 
+  /**
+  * Adds constraint to table
+  **/
+  function user_alter_table_constraint_post($req){
+    $statement_to_execute = "";
+    $statement_to_execute = _get_constraint_statement($req);
+    return executeQuery($constraint);
   }
 
+  /**
+  * Drops constraint from table
+  **/
   function user_alter_table_constraint_delete($req){
     $table_name = array_key_exists('table-name', $req) ? $req['table-name'] : '';
     $column_name = array_key_exists('constraint-name', $req) ? $req['constraint-name'] : '';
     $drop_constraint_statement = "ALTER TABLE $table_name
       DROP CONSTRAINT $column_name";
     return executeQuery($drop_constraint_statement);
+  }
+
+
+  /** PRIVATE FUNCTIONS **/
+
+  function _get_constraint_statement($req){
+    $statement_to_execute = "";
+    $constraint = json_decode($req['cons'], true);
+    switch ($req['constraint-type']){
+      case "P":
+        $statement_to_execute = _primary_keys($constraint);
+        break;
+      case "F":
+        $statement_to_execute = _foreign_keys($constraint);
+        break;
+      case "U":
+        $statement_to_execute = _unique_keys($constraint);
+        break;
+      case "C":
+        $statement_to_execute = _check_constraint($constraint);
+        break;
+      case "N":
+        $statement_to_execute = _not_null_constraint($constraint);
+        break;
+    }
+    return $statement_to_execute;
+  }
+
+  function _foreign_keys($foreign_keys){
+		$foreign_key_statement = "";
+		foreach($foreign_keys as $key){
+			$deferrable = "";
+			if($key['deferrable']){
+				$deferrable = "DEFERRABLE INITIALLY DEFERRED";
+			}
+			$foreign_key_statement = $foreign_key_statement . " CONSTRAINT " . $key['constraintName'] . " FOREIGN KEY (" . $key['tableCol'] . ") REFERENCES " . $key['refTable'] . "(" . $key['refCol'] . ") " . $deferrable . ",
+			";
+		}
+		$foreign_key_statement = rtrim($foreign_key_statement, ",
+		");
+		return $foreign_key_statement;
+	}
+
+  function _primary_keys($primary_keys){
+		$primary_key_statement = "";
+		$keys = "";
+		foreach($primary_keys['cols'] as $col){
+			$keys = $keys . $col . ",";
+		}
+		$keys = rtrim($keys, ",");
+		$primary_key_statement = " CONSTRAINT " . $primary_keys['constraintName'] . " PRIMARY KEY (" . $keys . "),
+		";
+		return $primary_key_statement;
+	}
+
+  function _unique_keys($unq_key){
+    $unique_key_statement = "";
+    $keys = "";
+    foreach($unq_key['cols'] as $col){
+			$keys = $keys . $col . ",";
+		}
+    $keys = rtrim($keys, ",");
+    $unique_key_statement = " CONSTRAINT " . $unq_key['constraintName'] . " UNIQUE (" . $keys . ")
+    ";
+    return $unique_key_statement;
+  }
+
+  function _check_constraint($chk_cons){
+    $check_constraint_statement = "";
+    $check_constraint_statement = " CONSTRAINT " . $chk_cons['constraintName'] . " CHECK (" . $chk_cons['checkCondition'] . ")
+    ";
+    return $check_constraint_statement;
+  }
+
+  function _not_null_constraint($nn_cons){
+    $not_null_constraint_statement = "";
+    $not_null_constraint_statement = " CONSTRAINT " . $nn_cons['constraintName'] . " CHECK (" . $nn_cons['columnName'] . "IS NOT NULL)
+    ";
+    return $not_null_constraint_statement;
   }
 
 ?>

581f6225855c784cb7110e4e999f673326a60931
Tyler Freeman <tyler.freeman@modea.com>
2015-04-06 21:13:28 -0400

    Fixed mispelling

--- End_points/user_alter_column_name.php
+++ End_points/user_alter_column_name.php
@@ -5,8 +5,8 @@ function user_alter_column_name_post($req){
   $column_to_rename = $req['column-name'];
   $new_column_name = $req['new-column-name'];
   $rename_column_statement = "";
-  $rename_column_statement = "ALTER TABLE $table_name 
-    RENAME $column_to_rename TO $new_table_name";
+  $rename_column_statement = "ALTER TABLE $table_name
+    RENAME $column_to_rename TO $new_column_name";
   executeQuery($rename_column_statement);
 }
 

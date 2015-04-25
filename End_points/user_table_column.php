a9b194e1bf0bd0148d9e4d0b08d3242c0f494dd1
Tyler Freeman <tyler.freeman@modea.com>
2015-04-25 15:30:51 -0400

    Added ability to get user table info by table name through user_table_info post request

--- End_points/user_table_column.php
+++ End_points/user_table_column.php
@@ -16,7 +16,8 @@
 	}
 
 	function user_table_column_get_constraints_by_column_name($table_name){
-		$query = "SELECT cols.table_name, cols.column_name, cols.position, cons.status, cons.owner, cons.constraint_type, cons.constraint_name
+		$query = "SELECT cols.table_name, cols.column_name, cols.position, cons.status,
+											cons.owner, cons.constraint_type, cons.search_condition, cons.constraint_name
 			FROM user_constraints cons, user_cons_columns cols
 			WHERE cols.table_name = '$table_name'
 			AND cons.constraint_name = cols.constraint_name

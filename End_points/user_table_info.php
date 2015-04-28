fc2ea91398f6a9530e0dab9b20286a771525714e
Tyler Freeman <tyler.freeman@modea.com>
2015-04-27 21:46:33 -0400

    resolved conflicts

--- End_points/user_table_info.php
+++ End_points/user_table_info.php
@@ -4,11 +4,7 @@ function user_table_info_post($req){
   $table_name = $req['table-name'];
   $constraint_type = $req['constraint-name'];
   $table =array();
-  $table['TABLE_COLS'] = user_table_column_get($table_name);
-  $table['TABLE_PRIMARY_KEY'] = user_schema_get_table_primary_key($table_name);
-  $table['TABLE_FOREIGN_KEY'] = user_schema_get_table_foreign_key($table_name);
-  $table['TABLE_TRIGGERS'] = user_schema_get_table_triggers($table_name);
-  $table['TABLE_CONSTRAINTS'] = get_table_constraints($table_name);
+  $table['TABLE_CONSTRAINTS'] = get_table_constraints($table_name, $constraint_type);
   return $table;
 }
 

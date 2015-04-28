045b1c5ed8baeefdf1ea11f02880b0e2f0179f47
Tyler Freeman <tyler.freeman@modea.com>
2015-04-27 21:39:21 -0400

    Reverted

--- End_points/user_schema.php
+++ End_points/user_schema.php
@@ -29,7 +29,7 @@
 		return $primary_keys;
 	}
 
-	function user_schema_get_table_foreign_key($table_name){
+	function user_schema_get_table_foreign_key($table_name){//
 		$results = executeQuery("SELECT c.constraint_name, a.table_name, a.column_name, a.table_name as reference_table, a.column_name as reference_column, c.deferrable, c.DEFERRED, c.DELETE_RULE
                 FROM user_cons_columns a
                 JOIN user_constraints c ON a.owner = c.owner

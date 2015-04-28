bc051bdb4885e3e8b923d4ca6ee96fc381784839
Tyler Freeman <tyler.freeman@modea.com>
2015-04-28 02:43:28 -0400

    hack

--- End_points/user_table_info.php
+++ End_points/user_table_info.php
@@ -10,14 +10,14 @@ function user_table_info_post($req){
 
 function get_table_constraints($table_name, $constraint_name){
   return executeQuery("SELECT c.constraint_name, c.constraint_type, a.table_name, a.column_name,
-                            r.table_name as reference_table, r.column_name as reference_column, c.deferrable,
+                            r.table_name as reference_table, a.column_name as reference_column, c.deferrable,
                             c.DEFERRED, c.DELETE_RULE, c.search_condition, c.status
                 FROM user_cons_columns a
                 JOIN user_constraints c ON a.owner = c.owner
                     AND a.constraint_name = c.constraint_name
-                LEFT OUTER JOIN (SELECT table_name, column_name, constraint_name FROM user_constraints NATURAL JOIN user_cons_columns) r
+                LEFT OUTER JOIN (SELECT table_name, constraint_name FROM user_constraints) r
                   ON r.constraint_name = c.r_constraint_name
-								WHERE a.table_name = '$table_name' AND c.constraint_name = '$constraint_name'");
+								WHERE a.table_name = '$table_name' AND c.constraint_name = '$constraint_name';");
 }
 
 ?>

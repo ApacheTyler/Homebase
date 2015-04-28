992e73c24367adea055fec038fb4532985e7dde1
Tyler Freeman <tyler.freeman@modea.com>
2015-04-27 21:44:58 -0400

    resolved conflicts

--- End_points/user_table_info.php
+++ End_points/user_table_info.php
@@ -2,6 +2,7 @@
 
 function user_table_info_post($req){
   $table_name = $req['table-name'];
+  $constraint_type = $req['constraint-name'];
   $table =array();
   $table['TABLE_COLS'] = user_table_column_get($table_name);
   $table['TABLE_PRIMARY_KEY'] = user_schema_get_table_primary_key($table_name);
@@ -11,7 +12,7 @@ function user_table_info_post($req){
   return $table;
 }
 
-function get_table_constraints($table_name){
+function get_table_constraints($table_name, $constraint_type){
   return executeQuery("SELECT c.constraint_name, c.constraint_type, a.table_name, a.column_name,
                             r.table_name as reference_table, a.column_name as reference_column, c.deferrable,
                             c.DEFERRED, c.DELETE_RULE, c.search_condition, c.status
@@ -20,7 +21,7 @@ function get_table_constraints($table_name){
                     AND a.constraint_name = c.constraint_name
                 LEFT OUTER JOIN (SELECT table_name, constraint_name FROM user_constraints) r
                   ON r.constraint_name = c.r_constraint_name
-								WHERE a.table_name = '$table_name'");
+								WHERE a.table_name = '$table_name' AND c.constraint_name = '$constraint_type'");
 }
 
 ?>
